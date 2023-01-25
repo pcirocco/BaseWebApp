684b165d3db8eef7bda855077e1e3fd39e3035cd
Peter Cirocco <pcirocco@remfq07wml87.myfiosgateway.com>
2023-01-24 21:21:02 -0500

    First working instance

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,108 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function() {
+  //getTickets();
+  // searchWeather();
+})
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+const getWeather = (searchQuery) => {
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=imperial&appid=cba75f0bde502b5c80b2a16758a24928";
+
+  $.ajax(url, {success: function(data){
+    console.log(data)
+    $(".city").text(data.name)
+    $(".temp").text(data.main.temp)
+  }})
 }
+
+const searchWeather = () => {
+  const searchQuery = $(".search").val()
+  getWeather(searchQuery )
+}
+
+const getTickets = (ticketQuery) => {
+    const url = "http://localhost:5000/json"
+  
+    $.ajax({
+  
+      // Our sample url to make request 
+      url: url,
+      // Type of Request
+      type: "GET",
+      dataType: "json",
+      data: {
+        hs_ticket_id: ticketQuery
+      },
+
+      // Function to call when to
+      // request is ok 
+      success: function (data) {
+          var x = JSON.stringify(data);
+          console.log(x);
+          $(".ticket").text('Ticket Name: ' + JSON.stringify(data['results'][0]['properties']['subject']))
+          $(".priority").text('Ticket Priority: ' + JSON.stringify(data['results'][0]['properties']['hs_ticket_priority']))
+      },
+
+      // Error handling 
+      error: function (error) {
+          console.log(`Error ${error}`);
+      }
+  });
+}
+
+const searchTickets = () => {
+  const ticketQuery = $(".search").val()
+  getTickets(ticketQuery)
+  console.log('search tickets called' + ticketQuery)
+}
+
+
+
+
+// const getTickets = () => {
+//   $.ajax({
+//     // url: 'https://api.hubspot.com/crm/v3/objects/tickets/1378329465',
+//     url: 'http://localhost:5000/',
+//     type: 'GET',
+//     beforeSend: function (xhr) {
+//         xhr.setRequestHeader('Authorization', 'Bearer pat-na1-8832ed51-46b7-4558-b87f-83026c1590db');
+//         xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+//         xhr.setRequestHeader('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+//         xhr.setRequestHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//     },
+//     data: {},
+//     success: function () { },
+//     error: function () { },
+//     });
+// }
+
+
+// const getTickets = () => {
+//   //const url = "https://api.hubspot.com/crm/v3/objects/tickets/1378329465"
+//   const url = "http://localhost:5000/json"
+//   $.ajax(url, {success: function(data){
+//     console.log('the data receieved from local host is: ', data['results'])
+//     $(".ticket").text('Ticket name: ' + JSON.stringify(data['results'][0]['properties']['subject']))
+//   }})
+// }
+
+//const url = "https://api.hubspot.com/crm/v3/objects/tickets/1378329465"
+
+// console.log('in get tickets')
+// $.ajax(url, 
+//   {type: 'GET',
+//   dataType: 'json',
+//   contentType: "application/json", 
+//     data: {
+//       hs_ticket_id: ticketQuery
+//   }}, 
+
+//    {success: function(data){
+//       data.preventDefault()
+//       alert("function ran!")
+//       $(".ticket").text('hi')
+//     }},
+//     {error: function(err) {
+//       console.log(err.responseText)
+//     }})
+  //console.log('the data receieved from local host is: ', res['results'])
+  // $(".ticket").text('Ticket ID: ' + JSON.stringify(res['results'][0]['properties']['subject']))
+  // $(".ticket").text('Ticket ID: ' + JSON.stringify(data['results'][0]['properties']['subject']))
--- project/start.js
+++ project/start.js
@@ -1,22 +1,173 @@
+//'use strict';
+
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var requestify = require('requestify');
+var request = require('request')
+const cors = require('cors');
+const hubspot = require('@hubspot/api-client');
+const { response } = require('express');
+const fetch = require('node-fetch')
+
+//app.use(cors())
+
+// We are using our packages here
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+ extended: true})); 
+app.use(cors())
 
 app.set('port', (process.env.PORT || 5000));
 
+// var BASE_URL = 'https://api.hubspot.com/crm/v3/objects/tickets/'+hs_ticket_id+;
+// var API_OPTIONS = {
+//   headers: { accept: 'application/json',
+//               Authorization: 'Bearer pat-na1-8832ed51-46b7-4558-b87f-83026c1590db'
+//  },
+// };
+
+//may need to use this at some point
+//app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.use(express.static(__dirname));
+
 
 // views is directory for all template files
 app.set('views', __dirname + '/html');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+
+// app.get('/json', async function(req, res, next) {
+//   // res.render('pages/index')
+//   const hubspotClient = new hubspot.Client({
+//     accessToken: process.env.HS_TOKEN
+//   });
+//   const PublicObjectSearchRequest = {
+//       "filterGroups":[{
+//         "filters": [
+//           {
+//             "propertyName": "subject",
+//             "operator": "EQ",
+//               "value": "Test Ticket -- In-Shape"
+//           }]
+//       }]
+//   }
+
+//   try {
+//     const apiResponse = await hubspotClient.crm.tickets.searchApi.doSearch(PublicObjectSearchRequest);
+//     console.log(JSON.stringify(apiResponse, null, 2));
+//     const data = apiResponse
+//     //res.render('pages/index', {data: apiResponse})
+//     res.send(data)
+//     //res.send(JSON.stringify(data))
+//     //res.render('pages/index')
+
+//   } catch (e) {
+//     e.message === 'HTTP request failed'
+//       ? console.error(JSON.stringify(e.response, null, 2))
+//       : console.error(e)
+//   }
+
+  
+//   //res.render('pages/index')
+//   next();
+
+// })
+
+app.get('/json', async function(req, res, next) {
+  
+  const hubspotClient = new hubspot.Client({
+    accessToken: process.env.HS_TOKEN
+  });
+  const PublicObjectSearchRequest = {
+      "filterGroups":[{
+        "filters": [
+          {
+            "propertyName": "hs_ticket_id",
+            "operator": "EQ",
+              "value": req.query['hs_ticket_id']
+          }]
+      }]
+  }
+  //console.log(req.query['hs_ticket_id'])
+
+  try {
+    const apiResponse = await hubspotClient.crm.tickets.searchApi.doSearch(PublicObjectSearchRequest);
+    console.log(JSON.stringify(apiResponse, null, 2));
+    const data = apiResponse
+    //res.render('pages/index', {data: apiResponse})
+    res.send(data)
+    //res.send(JSON.stringify(data))
+    //res.render('pages/index')
+
+  } catch (e) {
+    e.message === 'HTTP request failed'
+      ? console.error(JSON.stringify(e.response, null, 2))
+      : console.error(e)
+  }
+
+  // res.render('pages/index')
+
+  
+  //res.render('pages/index')
+  next();
+
+})
+
+app.get('/', function(req, res) {
+  res.render('pages/index')
+})
+
+app.get('/about', function(request, response, next) {
+  response.render('pages/about');
+  next();
 });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
 
-// This file is what handles incoming requests and
-// serves files to the browser, or executes server-side code
+// app.get('/projects', function(request, response) {
+//   response.render('pages/projects');
+// });
+
+
+// app.get('/json', async function(req, res, next) {
+//   // res.render('pages/index')
+//   const hubspotClient = new hubspot.Client({
+//     accessToken: process.env.HS_TOKEN
+//   });
+//   const PublicObjectSearchRequest = {
+//       "filterGroups":[{
+//         "filters": [
+//           {
+//             "propertyName": "subject",
+//             "operator": "EQ",
+//               "value": "Test Ticket -- In-Shape"
+//           }]
+//       }]
+//   }
+
+//   try {
+//     const apiResponse = await hubspotClient.crm.tickets.searchApi.doSearch(PublicObjectSearchRequest);
+//     console.log(JSON.stringify(apiResponse, null, 2));
+//     const data = apiResponse
+//     //res.render('pages/index', {data: apiResponse})
+//     res.send(data)
+//     //res.send(JSON.stringify(data))
+//     //res.render('pages/index')
+
+//   } catch (e) {
+//     e.message === 'HTTP request failed'
+//       ? console.error(JSON.stringify(e.response, null, 2))
+//       : console.error(e)
+//   }
+ //res.render('pages/index')
+//  next();
+
+// })
